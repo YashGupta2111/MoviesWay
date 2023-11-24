81afee08a724f002502437cb804e5d87af8ae023
Yash Gupta <yashguptag114@gmail.com>
2023-11-24 17:33:58 +0530

    Initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="p-5">
+      <p className="text-center">
+       Yash Gupta
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
